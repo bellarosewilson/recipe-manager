6f28fae27a8c682caa4c90247576325281135a58
Bella Rose Wilson <bwilson9893@gmail.com>
2026-03-02 20:31:01 +0000

    + CI Test - Fixes

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -13,7 +13,7 @@ require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
   describe 'associations' do
-    it { should belong_to(:author).class_name('User') }
+    it { should belong_to(:author).class_name('User').required }
     it { should have_many(:recipe_ingredients).class_name('Ingredient') }
     it { should have_many(:steps) }
   end
